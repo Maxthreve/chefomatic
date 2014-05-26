c86e3309e5642e04b5660e9dfb10bdae573cb1d1
Maxthreve <Maxthreve@gmail.com>
2014-05-25 22:15:46 -0400

    intitial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Chefomatic::Application.routes.draw do
+  devise_for :dietitians
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
