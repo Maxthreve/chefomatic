4cee07f5141e20fc226e4921e96ede308cd0879a
Max's Pal <AdrianMaxBestPals@gmail.com>
2014-06-09 17:57:24 -0400

    ingredients and allergens link back and forth

--- app/controllers/allergens_controller.rb
+++ app/controllers/allergens_controller.rb
@@ -5,6 +5,10 @@ class AllergensController < ApplicationController
     redirect_to :back
   end
   
+  def show
+    @allergen = Allergen.find(params[:id])
+  end
+  
   private
   def allergen_params
     params.require(:allergen).permit(:name)
